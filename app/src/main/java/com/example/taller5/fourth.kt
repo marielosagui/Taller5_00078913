e1c55ada74c76a0638cff4753812af44c94963c4
marielosagui <00078913@uca.edu.sv>
2019-06-13 00:15:09 -0600

    Modificaciones

--- app/src/main/java/com/example/taller5/fourth.kt
+++ app/src/main/java/com/example/taller5/fourth.kt
@@ -9,20 +9,9 @@ import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_fourth.view.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [fourth.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [fourth.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class fourth : Fragment() {
 
     override fun onCreateView(
--- app/src/main/java/com/example/taller5/third.kt
+++ app/src/main/java/com/example/taller5/third.kt
@@ -11,20 +11,11 @@ import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_third.view.*
 
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [third.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [third.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
+
 class third : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
